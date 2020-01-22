ad39dd1539f729bfb740962ded7865fdfc48d503
tatsumoto-taka <tatsumoto-taka@dmm.com>
2020-01-22 17:19:00 +0900

    settings fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,12 +2,7 @@
   <v-container>
     <v-layout text-center wrap>
       <v-flex xs12>
-        <v-img
-          src='assets/logo.svg'
-          class="my-3"
-          contain
-          height="200"
-        />
+        <v-img src = ../assets/logo.svg class="my-3" contain height="200"/>
       </v-flex>
 
       <v-flex mb-4>
