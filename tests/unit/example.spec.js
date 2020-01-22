6f279bf2414d1c7dc055bd506c593e5e70450145
tatsumoto-taka <tatsumoto-taka@dmm.com>
2020-01-22 17:43:25 +0900

    add snapshot testing

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -6,6 +6,12 @@ import Vuetify from "vuetify";
 Vue.use(Vuetify);
 
 describe("HelloWorld.vue", () => {
+  it("renders the correct markup", () => {
+    const helloWorld = shallowMount(HelloWorld);
+
+    expect(helloWorld).toMatchSnapshot(); // これだけでスナップショットが作成されます！！！簡単！！
+  });
+
   it("renders props.msg when passed", () => {
     const msg = "Welcome to Vuetify";
     const wrapper = shallowMount(HelloWorld, {
