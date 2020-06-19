456d2544105ac7dc5bb00e1baccfbcef65988863
Takamichi Tatsumoto <TakamichiTatsumoto@gmail.com>
2020-06-19 16:53:59 +0900

    fix target blank

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
+          <a href="https://community.vuetifyjs.com" rel="noopener noreferrer"
             >Discord Community</a
           >
         </p>
